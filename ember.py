d8b74ce087b751021d3e79ecb0d809b7867636e0
Lorino <nemeth.lorin.dorina@students.jedlik.eu>
2023-01-18 11:34:43 +0100

    ember lista+class

---
+++ ember.py
@@ -0,0 +1,8 @@
+class Ember:
+    def __init__(self,sor):
+        adatok = sor.strip().split(";")
+        self.kutyanev = adatok[0]
+        self.nev = adatok[1]
+        self.telefonszam = adatok[2]
+        self.emailcim = adatok[3]
+        self.lakcim = adatok[4]
