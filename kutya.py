769d93230b0820442e0ae25f581daa1d1582b966
moldovan.andras@students.jedlik.eu <moldovan.andras@students.jedlik.eu>
2023-01-31 09:59:36 +0100

    fileolvas

--- kutya.py
+++ kutya.py
@@ -1,15 +1,15 @@
 class Kutya:
-    def __init__(self,sor):
-            adatok = sor.strip().split(";")
-            self.nev = adatok[0]
-            self.szuletes = adatok[1]
-            self.fajta = adatok[2]
-            self.termet = adatok[3]
-            self.nem = adatok[4]
-            self.ivar = adatok[5]
-            self.statusz = adatok [6]
+    def __init__(self, sor):
+        adatok = sor.strip().split(";")
+        self.nev = adatok[0]
+        self.szuletes = int(adatok[1])
+        self.fajta = adatok[2]
+        self.termet = adatok[3]
+        self.nem = adatok[4]
+        self.ivar = adatok[5]
+        self.statusz = adatok [6]
 
-    def eletkor(self):
+    def eletkor(self, kutyak):
         eletkor = ""
         for kutya in kutyak:
             if kutya.szuletes >= 2020:
