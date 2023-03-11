1d95cb22409aab7ac1fae827dadd46cdf3acc1e4
bandi328 <moldovan.andras@students.jedlik.eu>
2023-03-11 18:48:35 +0100

    background

--- html/menu.js
+++ html/menu.js
@@ -5,10 +5,14 @@ function Nav() {
     document.getElementById("mySidebar").style.width = "250px";
     document.getElementById("main").style.marginLeft = "250px";
     document.querySelector("body").style.background = "rgba(0, 0, 0, 0.2)";
+    document.querySelector(".container").style = "filter: blur(5px)";
+    document.querySelector(".container").style = "-webkit-filter: blur(5px)";
   }
   else if (sideBarWidth !== "0px"){
     document.getElementById("mySidebar").style.width = "0px";
     document.getElementById("main").style.marginLeft = "0px";
     document.querySelector("body").style.background = "none";
+    document.querySelector(".container").style = "filter: blur(0px)";
+    document.querySelector(".container").style = "-webkit-filter: none";
   }
 }
